282b2171e44604062875b1d4302710ea653c91d3
samanthadeysel <samanthadeysel1@outlook.com>
2026-05-27 14:05:48 +0200

    Revert "colours and layout"
    This reverts commit 19b5d989b738bed1eb406d62edf038a36ac80835.

--- Tempo/app/src/main/java/com/example/tempo/RegisterFragment.kt
+++ Tempo/app/src/main/java/com/example/tempo/RegisterFragment.kt
@@ -4,22 +4,14 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Button
-import android.widget.TextView
 
 class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         val mainActivity = activity as MainActivity
 
         view.findViewById<Button>(R.id.btnSignup).setOnClickListener {
-            // After successful registration, go to Home and show bottom nav
-            mainActivity.navigateTo(HomeFragment(), showBottomNav = true)
-        }
-
-        view.findViewById<TextView>(R.id.tvLoginLink).setOnClickListener {
-            // Back to login, hide bottom nav
-            mainActivity.navigateTo(LoginFragment(), showBottomNav = false)
+            mainActivity.navigateTo(HomeFragment())
         }
     }
 }
